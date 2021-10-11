2a99234e5b96ebe0d7105245d165e806515fc27e
nicholas shawn freeman <nick_freeman65@yahoo.com>
2021-10-11 16:32:26 -0500

    commit tree 777e1bfa1bbde5740df340f611d92f876ac16160

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(errand = 'go to the office') {
+    return `This Monday, I will ${errand}.`;
+}
+
+function wrapAdjective(symbol = '*') {
+    return function (result = 'special') {
+        const emphatic = `You are ${symbol}${result}${symbol}!`
+        return emphatic;
+    }
+}
+
+console.log(saturdayFun());
+console.log(mondayWork());
+console.log(wrapAdjective()('a hard worker'));
+console.log(wrapAdjective('||')('a dedicated programmer'));
